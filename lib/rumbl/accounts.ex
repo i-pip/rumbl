be10c7e96e35b3e0d93a5b6d225960d50e29f7ca
pip <jrphiri@gmail.com>
2021-05-05 09:47:38 +0200

    Using Repository to add data

--- lib/rumbl/accounts.ex
+++ lib/rumbl/accounts.ex
@@ -4,22 +4,17 @@ defmodule Rumbl.Accounts do
   """
 
   alias Rumbl.Accounts.User
+  alias Rumbl.Repo
 
   def list_users do
-    [
-      %User{id: "1", name: "Phillip", username: "i-pip"},
-      %User{id: "2", name: "George", username: "georgebuthelezi"},
-      %User{id: "3", name: "TJ", username: "tj"}
-    ]
+    Repo.all(User)
   end
 
   def get_user(id) do
-    Enum.find(list_users(), fn map -> map.id == id end)
+    Repo.get!(User, id)
   end
 
   def get_user_by(params) do
-    Enum.find(list_users(), fn map ->
-      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
-    end)
+    Repo.get_by!(User, params)
   end
 end
--- lib/rumbl/accounts/user.ex
+++ lib/rumbl/accounts/user.ex
@@ -1,3 +1,13 @@
 defmodule Rumbl.Accounts.User do
-  defstruct [:id, :name, :username]
+  use Ecto.Schema
+  import Ecto.Changeset
+  # defstruct [:id, :name, :username] - No longer needed
+  # schema declaration below auto declares above struct
+
+  schema "users" do
+    field :name, :string
+    field :username, :string
+
+    timestamps()
+  end
 end
